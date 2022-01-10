234e973fd951c31cf4990b7d7efd7047a9fd2ef3
jmscoding <johannes.seitz@outlook.de>
2022-01-10 17:04:18 +0100

    Second

--- database.py
+++ database.py
@@ -28,3 +28,38 @@ class Database:
     def drop_col(self, col):
         drop_col = self.db[col]
         drop_col.drop()
+    
+    def update_one(self, col, query, new):
+        up_col_one = self.db[col]
+        x = up_col_one(query, new)
+
+    def find_one(self, col):
+        fin_one = self.db[col]
+        elems = [x for x in fin_one.find()]
+        return(elems)
+
+
+# Test Database Class
+#'''
+import json
+
+def load_bin(file):
+  with open(file, "rb") as f:
+    data = json.load(f)
+  return(data)
+
+
+if __name__ ==  "__main__":
+    test_db = Database(mongo_db="knowledgebase")
+
+    # Test include new elem to knowledgebase
+    '''
+    d = load_bin('/home/js/Desktop/COAMiner/src/servicesWin10.json')
+    win_10_processes = {
+        "_id": "win10process",
+        "data": d
+    }
+    test_db.insert_one_elem(col="process", elem=win_10_processes)
+    '''
+
+# '''
