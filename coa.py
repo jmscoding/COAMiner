4795444070c75343030b524347851f4e2219fa49
jmscoding <johannes.seitz@outlook.de>
2022-01-11 15:11:05 +0100

    Update Coa Class

--- coa.py
+++ coa.py
@@ -1,9 +1,26 @@
 import json
 
 class Coa:
-    def __init__(self, data=None, id=None) -> None:
-        self.data = data
+    def __init__(self, 
+                id=None, 
+                cve=None, 
+                title=None, 
+                article_id=None, 
+                author=None, 
+                date=None, 
+                blogname=None, 
+                coa=None) -> None:
         if id is not None:
             self.data['_id'] = id
+        self.enums = {
+            "cve": cve
+        }
+        self.article = {
+            "id": article_id,
+            "title": title,
+            "author": author,
+            "date": date,
+            "coa": coa
+        }
 
     
